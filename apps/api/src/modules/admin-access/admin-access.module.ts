03023e62951422ac40a202b7d4e5525429350d88
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-10 20:29:11 +0700

    feat(api): wire admin invitation lifecycle management

--- apps/api/src/modules/admin-access/admin-access.module.ts
+++ apps/api/src/modules/admin-access/admin-access.module.ts
@@ -3,12 +3,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
 import { AdminAccessController } from './admin-access.controller';
 import { AdminAccessService } from './admin-access.service';
+import { AdminInvitationAdminController } from './admin-invitation-admin.controller';
+import { AdminInvitationAdminService } from './admin-invitation-admin.service';
 import { AdminInvitationController } from './admin-invitation.controller';
 import { AdminInvitationService } from './admin-invitation.service';
 
 @Module({
   imports: [DatabaseModule, JwtModule.register({})],
-  controllers: [AdminAccessController, AdminInvitationController],
-  providers: [AdminAccessService, AdminInvitationService],
+  controllers: [AdminAccessController, AdminInvitationController, AdminInvitationAdminController],
+  providers: [AdminAccessService, AdminInvitationService, AdminInvitationAdminService],
 })
 export class AdminAccessModule {}
