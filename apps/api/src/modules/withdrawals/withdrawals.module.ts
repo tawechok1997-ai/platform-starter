f165c7821798c4478338500e9e5ecc6de83d834b
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-11 01:40:19 +0700

    feat(withdrawals): register staged payout workflow

--- apps/api/src/modules/withdrawals/withdrawals.module.ts
+++ apps/api/src/modules/withdrawals/withdrawals.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
+import { StorageModule } from '../storage/storage.module';
+import { WithdrawalWorkflowController } from './withdrawal-workflow.controller';
+import { WithdrawalWorkflowService } from './withdrawal-workflow.service';
 import { WithdrawalsController } from './withdrawals.controller';
 import { WithdrawalsService } from './withdrawals.service';
 
 @Module({
-  imports: [DatabaseModule, JwtModule.register({})],
-  controllers: [WithdrawalsController],
-  providers: [WithdrawalsService],
-  exports: [WithdrawalsService],
+  imports: [DatabaseModule, JwtModule.register({}), StorageModule],
+  controllers: [WithdrawalsController, WithdrawalWorkflowController],
+  providers: [WithdrawalsService, WithdrawalWorkflowService],
+  exports: [WithdrawalsService, WithdrawalWorkflowService],
 })
 export class WithdrawalsModule {}
