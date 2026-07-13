7f02ef8c810c4b20313b0f97eb8c5290174d33fd
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-14 02:11:01 +0700

    feat(risk): wire withdrawal risk enforcement

--- apps/api/src/modules/withdrawals/withdrawals.module.ts
+++ apps/api/src/modules/withdrawals/withdrawals.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
+import { RiskAlertsModule } from '../risk-alerts/risk-alerts.module';
 import { StorageModule } from '../storage/storage.module';
+import { WithdrawalRiskEnforcementService } from './withdrawal-risk-enforcement.service';
 import { WithdrawalWorkflowController } from './withdrawal-workflow.controller';
 import { WithdrawalWorkflowService } from './withdrawal-workflow.service';
 import { WithdrawalsController } from './withdrawals.controller';
 import { WithdrawalsService } from './withdrawals.service';
 
 @Module({
-  imports: [DatabaseModule, JwtModule.register({}), StorageModule],
+  imports: [DatabaseModule, JwtModule.register({}), StorageModule, RiskAlertsModule],
   controllers: [WithdrawalsController, WithdrawalWorkflowController],
-  providers: [WithdrawalsService, WithdrawalWorkflowService],
+  providers: [WithdrawalsService, WithdrawalWorkflowService, WithdrawalRiskEnforcementService],
   exports: [WithdrawalsService, WithdrawalWorkflowService],
 })
 export class WithdrawalsModule {}
