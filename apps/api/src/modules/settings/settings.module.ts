5751b87971493ec08de9bb5883bb79487ace2fa6
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-14 03:12:51 +0700

    feat(cms): register binary asset lifecycle

--- apps/api/src/modules/settings/settings.module.ts
+++ apps/api/src/modules/settings/settings.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
+import { StorageModule } from '../storage/storage.module';
+import { CmsAssetsController } from './cms-assets.controller';
+import { CmsAssetsService } from './cms-assets.service';
 import { SettingsController } from './settings.controller';
 import { SettingsService } from './settings.service';
 
 @Module({
-  imports: [DatabaseModule, JwtModule.register({})],
-  controllers: [SettingsController],
-  providers: [SettingsService],
-  exports: [SettingsService],
+  imports: [DatabaseModule, JwtModule.register({}), StorageModule],
+  controllers: [SettingsController, CmsAssetsController],
+  providers: [SettingsService, CmsAssetsService],
+  exports: [SettingsService, CmsAssetsService],
 })
 export class SettingsModule {}
