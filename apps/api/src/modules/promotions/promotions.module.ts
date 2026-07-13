100c331766d0f3495ce3bbfe22d99e52b1a1ab12
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-14 01:23:33 +0700

    feat(promotions): register promotion domain repository

--- apps/api/src/modules/promotions/promotions.module.ts
+++ apps/api/src/modules/promotions/promotions.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
+import { PromotionDomainRepository } from './promotion-domain.repository';
 import { PromotionsController } from './promotions.controller';
 import { PromotionsService } from './promotions.service';
 
 @Module({
   imports: [DatabaseModule, JwtModule.register({})],
   controllers: [PromotionsController],
-  providers: [PromotionsService],
+  providers: [PromotionsService, PromotionDomainRepository],
 })
 export class PromotionsModule {}
