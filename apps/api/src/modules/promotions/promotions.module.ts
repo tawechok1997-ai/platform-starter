450e3dbb83dbd958dab782ec288d0b4b6197308a
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-14 21:59:02 +0700

    refactor(r007): register promotions query service

--- apps/api/src/modules/promotions/promotions.module.ts
+++ apps/api/src/modules/promotions/promotions.module.ts
@@ -3,11 +3,12 @@ import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
 import { PromotionDomainRepository } from './promotion-domain.repository';
 import { PromotionsController } from './promotions.controller';
+import { PromotionsQueryService } from './promotions-query.service';
 import { PromotionsService } from './promotions.service';
 
 @Module({
   imports: [DatabaseModule, JwtModule.register({})],
   controllers: [PromotionsController],
-  providers: [PromotionsService, PromotionDomainRepository],
+  providers: [PromotionsService, PromotionsQueryService, PromotionDomainRepository],
 })
 export class PromotionsModule {}
