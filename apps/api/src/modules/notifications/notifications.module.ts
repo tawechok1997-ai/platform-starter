677e174536e510459e3535fbe80ee431ead04491
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-14 20:31:25 +0700

    refactor(r007): register notification query and command services

--- apps/api/src/modules/notifications/notifications.module.ts
+++ apps/api/src/modules/notifications/notifications.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
+import { NotificationsCommandService } from './notifications-command.service';
 import { NotificationsController } from './notifications.controller';
+import { NotificationsQueryService } from './notifications-query.service';
 import { NotificationsService } from './notifications.service';
 
 @Module({
   imports: [DatabaseModule, JwtModule.register({})],
   controllers: [NotificationsController],
-  providers: [NotificationsService],
+  providers: [NotificationsService, NotificationsQueryService, NotificationsCommandService],
 })
 export class NotificationsModule {}
