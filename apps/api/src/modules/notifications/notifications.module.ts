37a26d8ec4824ca0cd0c1680022ebb82683dc6d8
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-15 06:41:01 +0700

    refactor(r010): register notification feed read repository

--- apps/api/src/modules/notifications/notifications.module.ts
+++ apps/api/src/modules/notifications/notifications.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
+import { NotificationFeedReadRepository } from './notification-feed-read.repository';
 import { NotificationsCommandService } from './notifications-command.service';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsQueryService } from './notifications-query.service';
@@ -9,6 +10,11 @@ import { NotificationsService } from './notifications.service';
 @Module({
   imports: [DatabaseModule, JwtModule.register({})],
   controllers: [NotificationsController],
-  providers: [NotificationsService, NotificationsQueryService, NotificationsCommandService],
+  providers: [
+    NotificationsService,
+    NotificationsQueryService,
+    NotificationsCommandService,
+    NotificationFeedReadRepository,
+  ],
 })
 export class NotificationsModule {}
