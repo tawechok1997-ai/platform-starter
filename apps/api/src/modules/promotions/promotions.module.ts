fe3da82a10088a290fcbe1fd013241252283323c
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-14 22:11:05 +0700

    refactor(r007): register promotion claim command service

--- apps/api/src/modules/promotions/promotions.module.ts
+++ apps/api/src/modules/promotions/promotions.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
 import { BonusLifecycleCommandService } from './bonus-lifecycle-command.service';
+import { PromotionClaimCommandService } from './promotion-claim-command.service';
 import { PromotionDomainRepository } from './promotion-domain.repository';
 import { PromotionsController } from './promotions.controller';
 import { PromotionsQueryService } from './promotions-query.service';
@@ -10,7 +11,13 @@ import { PromotionsService } from './promotions.service';
 @Module({
   imports: [DatabaseModule, JwtModule.register({})],
   controllers: [PromotionsController],
-  providers: [PromotionsService, PromotionsQueryService, BonusLifecycleCommandService, PromotionDomainRepository],
-  exports: [PromotionsQueryService, BonusLifecycleCommandService],
+  providers: [
+    PromotionsService,
+    PromotionsQueryService,
+    PromotionClaimCommandService,
+    BonusLifecycleCommandService,
+    PromotionDomainRepository,
+  ],
+  exports: [PromotionsQueryService, PromotionClaimCommandService, BonusLifecycleCommandService],
 })
 export class PromotionsModule {}
