0afc0a97792d068fe9252076debc1048a02a6b08
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-14 07:57:38 +0700

    feat(support): wire binary attachment storage

--- apps/api/src/modules/support/support.module.ts
+++ apps/api/src/modules/support/support.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
+import { StorageModule } from '../storage/storage.module';
+import { SupportAttachmentsService } from './support-attachments.service';
 import { SupportController } from './support.controller';
 import { SupportService } from './support.service';
 
 @Module({
-  imports: [DatabaseModule, JwtModule.register({})],
+  imports: [DatabaseModule, JwtModule.register({}), StorageModule],
   controllers: [SupportController],
-  providers: [SupportService],
+  providers: [SupportService, SupportAttachmentsService],
 })
 export class SupportModule {}
