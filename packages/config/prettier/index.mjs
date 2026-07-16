242e4c3b0ab6fb1251a97c70ab2c14ec92ffdcc5
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-16 10:30:38 +0700

    chore: add shared Prettier config

---
+++ packages/config/prettier/index.mjs
@@ -0,0 +1,11 @@
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
