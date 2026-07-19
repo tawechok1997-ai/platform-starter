0c6184c3f3cac84e0474fdf2fcc1aa93ca6dda90
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-20 06:58:52 +0700

    connect provider simulator to wallet module

--- apps/api/src/modules/provider-simulator/provider-simulator.module.ts
+++ apps/api/src/modules/provider-simulator/provider-simulator.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { WalletModule } from '../wallet/wallet.module';
 import { ProviderSimulatorController } from './provider-simulator.controller';
 import { ProviderSimulatorService } from './provider-simulator.service';
 
 @Module({
+  imports: [WalletModule],
   controllers: [ProviderSimulatorController],
   providers: [ProviderSimulatorService],
   exports: [ProviderSimulatorService],
