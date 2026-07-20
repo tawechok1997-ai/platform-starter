d5faf62aebbf8cfe1e83679ec856385b9b2c1203
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-21 04:21:44 +0700

    refactor(api): use shared JWT module in topups

--- apps/api/src/modules/topups/topups.module.ts
+++ apps/api/src/modules/topups/topups.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtAuthModule } from '../../common/security/jwt-auth.module';
 import { DatabaseModule } from '../../database/database.module';
 import { StorageModule } from '../storage/storage.module';
 import { DepositWorkflowController } from './deposit-workflow.controller';
@@ -10,7 +10,7 @@ import { TopUpsController } from './topups.controller';
 import { TopUpsService } from './topups.service';
 
 @Module({
-  imports: [DatabaseModule, JwtModule.register({}), StorageModule, AdminAuthModule],
+  imports: [DatabaseModule, JwtAuthModule, StorageModule, AdminAuthModule],
   controllers: [TopUpsController, DepositWorkflowController],
   providers: [TopUpsService, DepositWorkflowService, BatchDepositWorkflowService],
   exports: [TopUpsService, DepositWorkflowService],
