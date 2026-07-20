980f7ab9e20d90e7a30a408034f00a0701a54244
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-21 03:44:58 +0700

    refactor(api): use shared JWT infrastructure in admin members

--- apps/api/src/modules/admin-members/admin-members.module.ts
+++ apps/api/src/modules/admin-members/admin-members.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from '../../database/database.module';
+import { JwtAuthModule } from '../../common/security/jwt-auth.module';
 import { AdminMembersController } from './admin-members.controller';
 import { AdminMembersService } from './admin-members.service';
 import { AdminMembersQueryService } from './admin-members-query.service';
 import { AdminMembersCommandService } from './admin-members-command.service';
 
 @Module({
-  imports: [DatabaseModule, JwtModule.register({})],
+  imports: [DatabaseModule, JwtAuthModule],
   controllers: [AdminMembersController],
   providers: [AdminMembersService, AdminMembersQueryService, AdminMembersCommandService],
   exports: [AdminMembersService, AdminMembersQueryService, AdminMembersCommandService],
