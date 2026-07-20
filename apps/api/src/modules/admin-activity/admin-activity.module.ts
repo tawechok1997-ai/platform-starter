b13562fc51c91d8dbc4438f9998ead45c078509d
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-21 03:54:22 +0700

    refactor(api): own compact activity query in admin activity module

--- apps/api/src/modules/admin-activity/admin-activity.module.ts
+++ apps/api/src/modules/admin-activity/admin-activity.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { JwtAuthModule } from '../../common/security/jwt-auth.module';
 import { DatabaseModule } from '../../database/database.module';
 import { AdminActivityController } from './admin-activity.controller';
+import { AdminActivityQueryService } from './admin-activity-query.service';
 import { AdminActivityService } from './admin-activity.service';
 
 @Module({
   imports: [DatabaseModule, JwtAuthModule],
   controllers: [AdminActivityController],
-  providers: [AdminActivityService],
-  exports: [AdminActivityService],
+  providers: [AdminActivityService, AdminActivityQueryService],
+  exports: [AdminActivityService, AdminActivityQueryService],
 })
 export class AdminActivityModule {}
