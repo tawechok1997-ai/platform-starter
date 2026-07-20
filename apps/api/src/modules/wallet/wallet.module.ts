3c7907182c2876da1a8718c86d70c6aa017c724e
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-21 04:31:32 +0700

    refactor: export admin ledger mutation owner

--- apps/api/src/modules/wallet/wallet.module.ts
+++ apps/api/src/modules/wallet/wallet.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtAuthModule } from '../../common/security/jwt-auth.module';
 import { DatabaseModule } from '../../database/database.module';
+import { AdminLedgerMutationService } from './admin-ledger-mutation.service';
 import { AdminWalletLedgerController } from './admin-wallet-ledger.controller';
 import { AdminWalletLedgerQueryService } from './admin-wallet-ledger-query.service';
 import { WalletController } from './wallet.controller';
@@ -9,7 +10,7 @@ import { WalletService } from './wallet.service';
 @Module({
   imports: [DatabaseModule, JwtAuthModule],
   controllers: [WalletController, AdminWalletLedgerController],
-  providers: [WalletService, AdminWalletLedgerQueryService],
-  exports: [WalletService],
+  providers: [WalletService, AdminLedgerMutationService, AdminWalletLedgerQueryService],
+  exports: [WalletService, AdminLedgerMutationService],
 })
 export class WalletModule {}
