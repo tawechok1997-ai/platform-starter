33b1f06dc664c837922bc323a2a0632d4651c15f
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-27 01:38:15 +0700

    style(member): load clone preview asset reuse pass

--- apps/web-member/app/clone-preview/page.tsx
+++ apps/web-member/app/clone-preview/page.tsx
@@ -1,5 +1,6 @@
 import ClonePreviewClient from './clone-preview-client';
 import './clone-preview.css';
+import './clone-preview-assets.css';
 
 export default function ClonePreviewPage() {
   return <ClonePreviewClient />;
