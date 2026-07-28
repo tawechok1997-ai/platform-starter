66d56ac5acacdcf3c6d8fff088fe951a9530fc94
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-29 04:02:36 +0700

    refactor(web-member): remove home-only game navigation owner

--- apps/web-member/app/member-home.tsx
+++ apps/web-member/app/member-home.tsx
@@ -11,7 +11,6 @@ import {
 import { CmsPopup } from './components/member-home-sections';
 import { DesktopHomeScaffold } from './components/member-home/desktop-home-scaffold';
 import { MobileV47Scaffold } from './components/member-home/mobile-v47-scaffold';
-import PublicHomeGameNavigationController from './components/member-home/public-home-game-navigation-controller';
 import { useMemberHomeData } from './hooks/use-member-home-data';
 
 type MemberHomeProps = {
@@ -98,7 +97,6 @@ export default function MemberHome(props: MemberHomeProps) {
 
   return (
     <>
-      <PublicHomeGameNavigationController />
       {homeContent}
       {props.cmsContent.popup.enabled && !popupClosed && (
         <CmsPopup content={props.cmsContent} primaryColor={props.primaryColor} onClose={closePopup} />
