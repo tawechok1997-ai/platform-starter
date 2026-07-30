3e6821ca835530a49ce656da40cdc36c7dae7dec
tawechok1997-ai <tawechok1997@gmail.com>
2026-07-31 01:28:44 +0700

    fix(api): run catalog normalization before Jest transforms

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/*.spec.ts'],
   testPathIgnorePatterns: process.env.FULL_SYSTEM_UNIT_ONLY === 'true' ? ['\\.db\\.spec\\.ts$'] : [],
+  globalSetup: '<rootDir>/jest.global-setup.js',
 };
