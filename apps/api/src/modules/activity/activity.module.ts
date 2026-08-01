5a85dbad62b98514b3eba0db70f59c08bc481dca
tawechok1997-ai <tawechok1997@gmail.com>
2026-08-02 06:54:55 +0700

    feat(activity): register configurable member activity system

--- apps/api/src/modules/activity/activity.module.ts
+++ apps/api/src/modules/activity/activity.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { JwtAuthModule } from '../../common/security/jwt-auth.module';
+import { DatabaseModule } from '../../database/database.module';
 import { AdminActivityModule } from '../admin-activity/admin-activity.module';
-import { OperationsController } from './operations.controller';
+import { AdminActivitiesController } from './admin-activities.controller';
+import { ActivityConfigService } from './activity-config.service';
+import { ActivityRepository } from './activity.repository';
 import { ActivityService } from './activity.service';
+import { MemberActivitiesController } from './member-activities.controller';
+import { MemberActivitiesService } from './member-activities.service';
+import { OperationsController } from './operations.controller';
 
 @Module({
-  imports: [JwtAuthModule, AdminActivityModule],
-  controllers: [OperationsController],
-  providers: [ActivityService],
-  exports: [ActivityService],
+  imports: [DatabaseModule, JwtAuthModule, AdminActivityModule],
+  controllers: [OperationsController, MemberActivitiesController, AdminActivitiesController],
+  providers: [ActivityService, ActivityConfigService, ActivityRepository, MemberActivitiesService],
+  exports: [ActivityService, ActivityConfigService, MemberActivitiesService],
 })
 export class ActivityModule {}
