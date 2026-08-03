b4a7db049ad92a5d682f490082b5a91e19f585d7
tawechok1997-ai <tawechok1997@gmail.com>
2026-08-03 10:30:24 +0700

    refactor(member-mobile): remove duplicate category state runtime

--- apps/web-member/app/member-home.tsx
+++ apps/web-member/app/member-home.tsx
@@ -15,7 +15,6 @@ import { DesktopGameFeedProvider } from './components/member-home/member-source-
 import HomeSidebarScrollController from './components/member-home/home-sidebar-scroll-controller';
 import MobileAuthenticatedAvatarRuntime from './components/mobile-home/mobile-authenticated-avatar-runtime';
 import MobileAuthenticatedHomeRuntime from './components/mobile-home/mobile-authenticated-home-runtime';
-import MobileCategoryTabRuntime from './components/mobile-home/mobile-category-tab-runtime';
 import MobileCouponPopupBridge from './components/mobile-home/mobile-coupon-popup-bridge';
 import MobileHomeGuidePreview from './components/mobile-home/mobile-home-guide-preview';
 import MobileHomeMotionRuntime from './components/mobile-home/mobile-home-motion-runtime';
@@ -70,7 +69,6 @@ export default function MemberHome(props: MemberHomeProps) {
         <MobileCouponPopupBridge />
         <MobileMemberMenuSourceBridge />
         <MobileAuthenticatedAvatarRuntime />
-        <MobileCategoryTabRuntime />
         <MobileHomeGuidePreview />
         <MobileScrollComfortGuard />
         <MobileHomeMotionRuntime contentVersion={mobileHomeMotionVersion(props.cmsContent)} />
